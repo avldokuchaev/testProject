f5f6004c432ee7d6de2868c00f89e88338934187
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-08 23:34:23 +0300

    Square and perimeter of room

---
+++ square_and_perimetr_of_room.py
@@ -0,0 +1,6 @@
+lenght_of_room = 16.7
+width_of_room = 12.5
+square_of_room = lenght_of_room * width_of_room
+perimeter_of_room = (lenght_of_room + width_of_room) * 2
+print("Площадь комнаты = ", square_of_room, "м.")
+print("Периметр комнаты = ",perimeter_of_room, "м.")
