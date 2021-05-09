682da62fd7574edc585b38414cea780b8bfc52e1
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-09 21:49:08 +0300

    Favorit icecream flavor enterbox + default

--- flavor_icecream_enterbox.py
+++ flavor_icecream_enterbox.py
@@ -1,4 +1,5 @@
 import easygui
-flavor_icecream = easygui.enterbox("Какой твой любимый вкус мороженого?")
+flavor_icecream = easygui.enterbox("Какой твой любимый вкус мороженого?",
+                                   default = "Шоколадное")
 your_favorit_flavor = easygui.msgbox("Ваш любимый вкус мороженого: " + flavor_icecream)
 print(your_favorit_flavor)
