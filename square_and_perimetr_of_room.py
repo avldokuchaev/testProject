2d74daeb432e256c36629885bf5b3f973efd018b
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-09 10:28:33 +0300

    fix square in m^2

--- square_and_perimetr_of_room.py
+++ square_and_perimetr_of_room.py
@@ -2,5 +2,5 @@ lenght_of_room = 16.7
 width_of_room = 12.5
 square_of_room = lenght_of_room * width_of_room
 perimeter_of_room = (lenght_of_room + width_of_room) * 2
-print("Площадь комнаты = ", square_of_room, "м.")
+print("Площадь комнаты = ", square_of_room, "м^2.")
 print("Периметр комнаты = ",perimeter_of_room, "м.")
