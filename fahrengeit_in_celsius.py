140e7cc1b3c300b0536c76531f8c669c5e680a1e
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-09 10:11:08 +0300

    Fahrengeit into Celsius

---
+++ fahrengeit_in_celsius.py
@@ -0,0 +1,5 @@
+print("Эта программа преобразует градусы Фаренгейта в градусы Цельсия")
+print("Введите температуру в градусах Фаренгейта: ")
+fahrengeit_degree = float(input())
+celsius_degree = (fahrengeit_degree - 32) * 5.0 / 9
+print("Это ", celsius_degree, "градусов Цельсия.")
