65bebd2d00e305b82a8ab00b8fd7cab43ca7ab3d
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-09 10:53:21 +0300

    Count of coins

---
+++ count_of_small_change.py
@@ -0,0 +1,6 @@
+count_of_50 = int(input("Сколько у вас монет по 50 копеек: "))
+count_of_10 = int(input("Сколько у вас монет по 10 копеек: "))
+count_of_5 = int(input("Сколько у вас монет по 5 копеек: "))
+count_of_1 = int(input("Сколько у вас монет по 1 копейке: "))
+count_of_coins = count_of_50 + count_of_10 + count_of_5 + count_of_1
+print("Количество монет:", count_of_coins)
