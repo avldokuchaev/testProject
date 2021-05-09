eb3fc8b53ccbf3fc497c24bb5a99a76a1e87590e
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-09 10:27:49 +0300

    Square of carpet in cm^2

---
+++ square_of_carpet.py
@@ -0,0 +1,4 @@
+lenght_of_room = float(input("Введите длину комнаты в сантиметрах: "))
+width_of_room = float(input("Введите ширину комнаты в сантиметрах: "))
+square_of_carpet = lenght_of_room * width_of_room
+print("Для покрытия комнаты необходим ковер площадью", square_of_carpet, "см^2.")
