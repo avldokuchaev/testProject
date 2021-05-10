a3dfc238e7390d3f5230c7160541804ea21e19c9
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-10 14:47:56 +0300

    sale programm full

--- sell_procent.py
+++ sell_procent.py
@@ -1,5 +1,14 @@
 # В магазине распродажа. На товары за 10 долларов и меньше скидка 10%, а на товары дороже 10 долларов
 # — 20 %. Напишите программу, которая будет запрашивать цену товара и показывать
 # размер скидки (10 или 20 %) и итоговую цену.
-
-
+cost_of_thing = float(input("Введите цену товара: "))
+if cost_of_thing <= 10:
+    cost_of_thing_with_saler = cost_of_thing - cost_of_thing * 0.1
+    cost_of_thing_salers = cost_of_thing * 0.1
+    print("Ваша скидка равна: ", cost_of_thing_salers)
+    print("Цена вашего товара: ", cost_of_thing_with_saler)
+if cost_of_thing > 10:
+    cost_of_thing_with_saler = cost_of_thing - cost_of_thing * 0.2
+    cost_of_thing_salers = cost_of_thing * 0.2
+    print("Ваша скидка равна: ", cost_of_thing_salers)
+    print("Цена вашего товара: ", cost_of_thing_with_saler)
