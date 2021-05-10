aa820459bb5d95f738140a4d6a2a185f50bb0439
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-10 14:55:05 +0300

    sale programm full finalized with round function

--- sell_procent.py
+++ sell_procent.py
@@ -3,12 +3,15 @@
 # размер скидки (10 или 20 %) и итоговую цену.
 cost_of_thing = float(input("Введите цену товара: "))
 if cost_of_thing <= 10:
+
     cost_of_thing_with_saler = cost_of_thing - cost_of_thing * 0.1
     cost_of_thing_salers = cost_of_thing * 0.1
-    print("Ваша скидка равна: ", cost_of_thing_salers)
+    print("Ваша скидка равна: 10%")
+    print("Ваша скидка равна: ", round(cost_of_thing_salers, 1))
     print("Цена вашего товара: ", cost_of_thing_with_saler)
 else:
     cost_of_thing_with_saler = cost_of_thing - cost_of_thing * 0.2
     cost_of_thing_salers = cost_of_thing * 0.2
-    print("Ваша скидка равна: ", cost_of_thing_salers)
+    print("Ваша скидка равна: 20%")
+    print("Ваша скидка равна: ", round(cost_of_thing_salers, 1))
     print("Цена вашего товара: ", cost_of_thing_with_saler)
