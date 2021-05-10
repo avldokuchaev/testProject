8323bf70c3befaa168b33df743e84b813b7b83ef
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-10 10:13:19 +0300

    relational operators

---
+++ relational_operators.py
@@ -0,0 +1,10 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+if num1 < num2:
+    print(num1, "меньше", num2)
+if num1 > num2:
+    print(num1, "больше", num2)
+if num1 == num2:
+    print(num1, "равно", num2)
+if num1 != num2:
+    print(num1, "не равно", num2)
