be0d5c53aec01b8a2b66f77c3feedbabf8c03762
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-11 17:19:12 +0300

    Associations condition programm with password

---
+++ association_conditions_with_passw.py
@@ -0,0 +1,12 @@
+password = input("Введите пароль: ")
+if password == "avldokuchaev":
+    print("Вы вошли в программу!")
+    age = int(input("Сколько вам лет? "))
+    grade = int(input("В каком кассе вы учитесь? "))
+    favorite_color = input("Какой ваш любимый цвет? ")
+    if age >= 8 and grade >= 3 and favorite_color == "Фиолетовый":
+        print("Вам можно играть в игру")
+    else:
+        print("Вы не подходите для игры!")
+else:
+    print("Пароль не верный")
