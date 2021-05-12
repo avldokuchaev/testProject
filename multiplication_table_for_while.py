1f5d5eafd397875cd367bd4b14c0396b879b256c
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-12 23:34:19 +0300

    Multiplication table with for loop ver 2

--- multiplication_table_for_while.py
+++ multiplication_table_for_while.py
@@ -1,9 +1,10 @@
 # Напишите программу, выводящую на экран таблицу умножения. Первым делом она должна
 # спрашивать, для какого числа требуется вывести таблицу.
 number_for_multipl = int(input("Введите число для таблицы умножения: "))
+number_1_for_multiple = int(input("Введите число до какого множителя: "))
 # for i in range(1, 11):
 #     print(i, "* ", number_for_multipl, "=", number_for_multipl * i)
 i = 0
-while i < 10:
+while i < number_1_for_multiple:
     i += 1
     print(i, "* ", number_for_multipl, "=", number_for_multipl * i)
