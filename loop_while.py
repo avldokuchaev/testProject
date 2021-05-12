6b49b7f80c7bc00fc981a3dc5494ec1c482a6a36
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-12 23:05:11 +0300

    Loop while example

---
+++ loop_while.py
@@ -0,0 +1,7 @@
+print("Введите 3 чтобы продолжить")
+some_input = input()
+while some_input == "3":
+    print("Спасибо за 3. Вы очень добры")
+    print("Введите 3 чтобы продолжить")
+    some_input = input()
+print("Это не 3. Я завершаю работу.")
