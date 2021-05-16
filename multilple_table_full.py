eb5dfc794d835c93a5d90cd05129aa3921aaa43d
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-16 19:57:57 +0300

    Multiplication table full with tab

--- multilple_table_full.py
+++ multilple_table_full.py
@@ -1,4 +1,4 @@
 for multiple in range(1, 11):
     for i in range(1, 11):
-        print(i, "* ", multiple, "=", multiple * i)
+        print(i, "* ", multiple, "=", multiple * i, end=" | \t")
     print()
