77764a71550f207c2dc578eeb43062f966c8b594
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-16 21:28:15 +0300

    Calorii in hot dog version 3 with stars

--- timer_retrogrdis.py
+++ timer_retrogrdis.py
@@ -1,6 +1,9 @@
 import time
 initial_time = int(input("Сколько секунд обратног отчета? "))
 for i in range(initial_time, 0, -1):
-    print(i)
+    print(i, end=" ")
+    for star in range(0, i):
+        print("*", end=" ")
+    print()
     time.sleep(1)
 print("Пуск!")
