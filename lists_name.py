8846de6e5563160e98b017311dacf7edeaa17113
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-05-23 10:11:29 +0300

    Name list append

---
+++ lists_name.py
@@ -0,0 +1,12 @@
+# Напишите программу,которая будет запрашивать у пользователя пять имен.
+# Имена должны сохраняться в списке и выводиться в конце программы.
+name_list = []
+print("Введите пять имен")
+name_list.append(input("Введите первое имя: "))
+name_list.append(input("Введите второе имя: "))
+name_list.append(input("Введите третье имя: "))
+name_list.append(input("Введите четвертое имя: "))
+name_list.append(input("Введите пятое имя: "))
+print("Это имена: ", end=" ")
+for names in name_list:
+    print(names, end=" ")
