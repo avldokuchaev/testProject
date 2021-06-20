8ad16dc2cc21463897fc6dd4f5deb1a1727aa471
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-06-20 13:50:42 +0300

    datetime present 1

---
+++ probnik.py
@@ -0,0 +1,8 @@
+# Добро пожаловать
+num_stars = input("Количество звезд: ")
+letternum = 1
+for i in num_stars:
+    print("Bukva", letternum, "budet", i)
+    letternum += 1
+else:
+    print("stroka pusta")
