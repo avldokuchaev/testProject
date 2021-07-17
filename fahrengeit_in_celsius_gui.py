a55a6749717edde1f6ab14c55255fdd7c699a042
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-17 11:04:09 +0300

    2 version with round

--- fahrengeit_in_celsius_gui.py
+++ fahrengeit_in_celsius_gui.py
@@ -2,5 +2,5 @@ import easygui
 hello_text = easygui.msgbox("Эта программа преобразует градусы Фаренгейта в градусы Цельсия")
 fahrengeit_degree = float(easygui.enterbox("Введите температуру в градусах Фаренгейта:"))
 celsius_degree = (fahrengeit_degree - 32) * 5.0 / 9
-result_text = easygui.msgbox("Результат в градусах Цельсия: " + str(celsius_degree) + " градуса")
+result_text = easygui.msgbox("Результат в градусах Цельсия: " + str(round(celsius_degree, 2)) + " градуса")
 print(result_text)
