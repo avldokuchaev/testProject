dee16570a9a7de1f636bebaf6af521758b2802e1
avldokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-19 12:23:14 +0300

    body square 2 version

--- body_square.py
+++ body_square.py
@@ -1,6 +1,9 @@
 # Рассчет площади поверхности тела
+import easygui
 
-height = float(input("Введите рост в сантиметрах: "))
-weight = float(input("Введите вес в килограммах: "))
+height = float(easygui.enterbox("Введите рост в сантиметрах: "))
+weight = float(easygui.enterbox("Введите вес в килограммах: "))
 square_body = 0.0167 * weight ** 0.5 * height ** 0.5
-print(round(square_body, 2))
+square_body_result = round(square_body, 2)
+message_result = easygui.msgbox("Площадь поверхности тела = " + str(square_body_result) + " кубических метров")
+print(message_result)
