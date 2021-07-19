e44afe7b6a1ac7dd03a0160998497d7eabcb009f
Andrei Dokuchaev <a.vl.dokuchaev@gmail.com>
2021-07-19 17:20:06 +0300

    12 version

--- body_square.py
+++ body_square.py
@@ -8,7 +8,7 @@ def square_body_chemotherapy(weight_float, height_float):
 height = float(input("Введите рост в сантиметрах: "))
 weight = float(input("Введите вес в килограммах: "))
 schema_therapy = input("Введите название схемы на латинице (AC): ")
-reduction_doze = float(input("Если нужна редукция дозы, введите цифру процентов, иначе введите 0: "))
+reduction_doze = int(input("Если нужна редукция дозы, введите цифру процентов, иначе введите \"0\": "))
 
 res = square_body_chemotherapy(height, weight)
 
@@ -17,17 +17,17 @@ if schema_therapy == "AC" and reduction_doze == 0:
     ciklophosphamid = 600
     doksorubicin_doza = doksorubicin * res
     ciklophosphamid_doza = ciklophosphamid * res
-    print("Площадь поверхности тела = " + str(res) + " квадратных метров")
-    print("Необходимая доза Доксорубицина = " + str(round(doksorubicin_doza, 0)) + " мг")
-    print("Необходимая доза Циклофосфамида = " + str(round(ciklophosphamid_doza, 0)) + " мг")
+    print(f"Площадь поверхности тела = {str(res)} квадратных метров")
+    print(f"Необходимая доза Доксорубицина = {str(round(doksorubicin_doza, 0))} мг")
+    print(f"Необходимая доза Циклофосфамида = {str(round(ciklophosphamid_doza, 0))} мг")
 elif schema_therapy == "AC" and reduction_doze != 0:
     doksorubicin = 60
     ciklophosphamid = 600
     doksorubicin_doza = (doksorubicin * res) - ((doksorubicin * res) * (reduction_doze / 100))
     ciklophosphamid_doza = (ciklophosphamid * res) - ((ciklophosphamid * res) * (reduction_doze / 100))
-    print("Площадь поверхности тела = " + str(res) + " квадратных метров")
-    print("Необходимая доза Доксорубицина = " + str(round(doksorubicin_doza, 0)) + " мг")
-    print("Необходимая доза Циклофосфамида = " + str(round(ciklophosphamid_doza, 0)) + " мг")
+    print(f"Площадь поверхности тела = {str(res)} квадратных метров")
+    print(f"Необходимая доза Доксорубицина (редуцированная на {reduction_doze}%) = {str(round(doksorubicin_doza, 0))} мг")
+    print(f"Необходимая доза Циклофосфамида (редуцированная на {reduction_doze}%) = {str(round(ciklophosphamid_doza, 0))} мг")
 else:
     print("Вы ввели не ту схему!")
 
